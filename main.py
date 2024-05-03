6611f42750501c9f95396515317e6576754c9ffa
Navdeep Singh <132869610+NavdeepSinghNegi999@users.noreply.github.com>
2024-05-03 15:43:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 showfileUploaderEncoding = False
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model("./ResNet50_model.keras", compile=False)
+    model = tf.keras.models.load_model("ResNet50_model.keras", compile=False)
     return model
 model = load_model()
 
