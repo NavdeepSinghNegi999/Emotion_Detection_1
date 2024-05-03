617bde6cf797671b4565ce3a2e8c37e3cd7e8496
Navdeep Singh <132869610+NavdeepSinghNegi999@users.noreply.github.com>
2024-05-03 21:15:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import tensorflow as tf
 import numpy as np
 import cv2
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
+import keras
+
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 showfileUploaderEncoding = False
 @st.cache(allow_output_mutation=True)
-def load_model():
+def load_my_model():
     model = keras.models.load_model("ResNet50_model.keras")
     return model
-model = load_model()
+model = load_my_model()
 
 
 
