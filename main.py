8ff3be000ce670377beaf11ff81795925d419ba0
Navdeep Singh <132869610+NavdeepSinghNegi999@users.noreply.github.com>
2024-05-03 22:25:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 showfileUploaderEncoding = False
 @st.cache(allow_output_mutation=True)
 def load_my_model():
-    model = tf.keras.models.load_weights("ResNet50_model.keras")
+    model = tf.keras.models.load_model("ResNet50_model.keras")
     return model
 model = load_my_model()
 
