7b8800651cdb496de6ecc0725040096e12768b2c
Navdeep Singh <132869610+NavdeepSinghNegi999@users.noreply.github.com>
2024-05-03 13:23:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfigura
 # showfileUploaderEncoding = False
 # @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model(r'ResNet50_model.keras')
+    model = tf.keras.models.load_model('ResNet50_model.keras')
     return model
 model = load_model()
 
