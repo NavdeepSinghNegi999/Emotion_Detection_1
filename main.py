29b4b4bd0db586fa5aa4dc513cde24f9adb33f5a
Navdeep Singh <132869610+NavdeepSinghNegi999@users.noreply.github.com>
2024-05-03 16:07:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ import tensorflow as tf
 import numpy as np
 import cv2
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
-import keras
+from keras import backend as K
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 showfileUploaderEncoding = False
+# @st.cache(allow_output_mutation=True)
+# def load_model():
+#     model = keras.models.load_model("ResNet50_model.keras")
+#     return model
+# model = load_model()
+
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = keras.models.load_model("ResNet50_model.keras")
-    return model
-model = load_model()
+    model = load_model("ResNet50_model.keras")
+    model._make_predict_function()
+    model.summary()
+    session = K.get_session()
+    return model, session
 
+model, session = load_model()
 
 def index_to_emotion(index):
     emotion_labels = {'angry': 0, 'disgust': 1, 'fear': 2, 'happy': 3, 'neutral': 4, 'sad': 5, 'surprise': 6}
